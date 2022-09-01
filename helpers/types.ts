89a602591c9df364852f96362737ee60398f0b15
wojtek <wojtekpietraszuk@gmail.com>
2022-09-01 19:50:34 +0200

    Add folder helper
    Folder for constants and ts types

---
+++ helpers/types.ts
@@ -0,0 +1,43 @@
+export enum Offer {
+  Category = 'category',
+  Technologies = 'technologies',
+  MinSalary = 'minSalary',
+  MaxSalary = 'maxSalary',
+  ExactSalary = 'exactSalary',
+  Location = 'location',
+  WorkingHour = 'workingHour',
+  Seniority = 'seniority',
+  Benefits = 'benefits',
+  JobTitle = 'jobTitle',
+  Description = 'description',
+  Obligation = 'obligation',
+  Requirements = 'requirements',
+  Advantages = 'advantages',
+  CompanyName = 'companyName',
+  City = 'city',
+  Street = 'street',
+  BuildingNumber = 'building',
+  HouseNumber = 'house',
+}
+
+export interface FormData {
+  category: string;
+  technologies: string[];
+  minSalary: string;
+  maxSalary: string;
+  exactSalary: string;
+  location: string;
+  workingHour: string;
+  seniority: string;
+  benefits: string;
+  jobTitle: string;
+  description: string;
+  obligation: string;
+  requirements: string;
+  advantages: string;
+  companyName: string;
+  city: string;
+  street: string;
+  building: string;
+  house: string;
+}
