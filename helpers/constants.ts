ebe7d27f6f9de0e92e8297add0191b83dd2e24c2
wojtek <wojtekpietraszuk@gmail.com>
2022-09-01 19:51:27 +0200

    Change name to plural

---
+++ helpers/constants.ts
@@ -0,0 +1,41 @@
+export const cities = [
+  'Warszawa',
+  'Kraków',
+  'Wrocław',
+  'Poznań',
+  'Trójmiasto',
+  'Białystok',
+  'Bielsko-Biała',
+  'Bydgoszcz',
+  'Częstochowa',
+  'Kielce',
+  'Lublin',
+  'Łódź',
+  'Olsztyn',
+  'Opole',
+  'Rzeszów',
+  'Szczecin',
+  'Toruń',
+  'Zielona góra',
+];
+
+export const seniorities = ['stażysta', 'junior', 'mid', 'senior'];
+
+export const technologies = [
+  'JavaScript',
+  'TypeScript',
+  'NextJS',
+  'React',
+  'GraphQL',
+  'tRPC',
+  'Angular',
+  'Vue',
+  'Node.js',
+  'Spring',
+  '.Net',
+  'SQL',
+  'Python',
+  'C++',
+  'PHP',
+  'Laravel',
+];
