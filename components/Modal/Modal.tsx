2747610188593d015b40c6e94bc5c727bfa08ba9
wojtek <wojtekpietraszuk@gmail.com>
2022-09-02 15:48:00 +0200

    Fix imports and improve styles

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-
-import { technologies, seniorities } from '../new-post/OfferDetails';
-import { cities } from '../new-post/CompanyInfo';
+import { technologies, seniorities, cities } from '../../helpers/constants';
 
 const Modal: React.FC<{ show: boolean; close: () => void }> = ({
   show,
@@ -16,7 +14,17 @@ const Modal: React.FC<{ show: boolean; close: () => void }> = ({
 
   const modalContent = show ? (
     <div className='absolute top-0 left-0 right-0 bg-white p-5 lg:block lg:static lg:p-0'>
-      <h3>Filtruj</h3>
+      <div className='flex items-center'>
+        <button
+          type='button'
+          onClick={close}
+          className='bg-gray-200 text-black py-3 px-6 rounded-lg'
+        >
+          Wstecz
+        </button>
+        <h3 className='text-2xl mx-auto'>Filtruj oferty</h3>
+      </div>
+      <hr className='w-full mt-6' />
       <div className='mt-3'>
         <p className='text-lg'>Technologie</p>
         <div className='flex flex-wrap items-center text-black col-start-2 col-end-3'>
@@ -62,7 +70,7 @@ const Modal: React.FC<{ show: boolean; close: () => void }> = ({
       </div>
       <button
         type='button'
-        className='bg-green-500 text-white px-4 py-1 rounded-lg mt-10 w-full'
+        className='bg-green-500 text-white py-3 rounded-lg mt-10 w-full'
         onClick={close}
       >
         Filtruj
