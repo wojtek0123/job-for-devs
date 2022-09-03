add0dd24439952aff3a083c9c762f0e5944a92d2
wojtek <wojtekpietraszuk@gmail.com>
2022-09-03 14:13:32 +0200

    Change layout to white and gray

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { NextPage } from 'next';
-import Image from 'next/image';
-import arrowIcon from '../public/icons/arrow-left-solid.svg';
 import Link from 'next/link';
 import { signIn } from 'next-auth/react';
 import React from 'react';
@@ -34,10 +32,12 @@ const Login: NextPage = () => {
   // };
 
   return (
-    <div className='flex flex-col bg-slate-800 p-10 text-white sm:rounded-lg absolute inset-0 justify-center items-center md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 max-w-md'>
+    <div className='flex flex-col bg-gray-200 p-10 text-black border border-gray-200 sm:rounded-lg absolute inset-0 justify-center items-center md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 max-w-md shadow-xl'>
       <Link href='/'>
-        <a className='bg-gray-200 border absolute top-4 left-4 text-black shadow px-4 py-1 rounded-lg ml-2 xl:ml-0 text-lg hover:bg-gray-300 transition-colors duration-300'>
-          <Image src={arrowIcon} alt='back' width={15} height={15} />
+        <a className='absolute top-4 left-4 inline-flex items-center bg-white border text-black shadow px-4 py-1 rounded-lg ml-2 xl:ml-0 text-lg hover:bg-gray-300 transition-colors duration-300'>
+          <svg className='w-4 h-4 fill-black' viewBox='0 0 448 512'>
+            <path d='M447.1 256c0 17.7-13.4 32-31.1 32H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25-6.3 6.25-14.5 9.35-22.7 9.35s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416c17.7 0 31.1 14.3 31.1 32z' />
+          </svg>
           <span className='ml-2'>back</span>
         </a>
       </Link>
@@ -46,7 +46,7 @@ const Login: NextPage = () => {
       </h1>
       <div className='flex flex-col justify-center w-full items-center'>
         <button
-          className='bg-white text-slate-800 w-full max-w-[13rem] py-2 my-2 rounded-lg hover:bg-slate-200 transition-colors duration-200'
+          className='bg-white text-black w-full max-w-[13rem] py-2 my-2 rounded-lg hover:bg-gray-300 transition-colors duration-300'
           type='button'
           // eslint-disable-next-line @typescript-eslint/no-misused-promises
           onClick={signInWithGithub}
@@ -54,7 +54,7 @@ const Login: NextPage = () => {
           Sign in with GitHub
         </button>
         <button
-          className='bg-white text-slate-800 w-full max-w-[13rem] py-2 my-2 rounded-lg hover:bg-slate-200 transition-colors duration-200'
+          className='bg-white text-black w-full max-w-[13rem] py-2 my-2 rounded-lg hover:bg-gray-300 transition-colors duration-300'
           type='button'
           // eslint-disable-next-line @typescript-eslint/no-misused-promises
           onClick={signInWithGoogle}
@@ -70,11 +70,11 @@ const Login: NextPage = () => {
             id='email'
             placeholder='email@example.com'
             ref={emailInputRef}
-            className='rounded-lg px-3 py-1 w-full max-w-xs text-slate-800'
+            className='rounded-lg px-3 py-1 w-full max-w-xs text-black'
           />
           <button
             type='submit'
-            className='bg-white text-slate-800 rounded-lg mt-7 py-1 cursor-pointer mb-4 w-full max-w-xs'
+            className='bg-gray-200 text-black rounded-lg mt-7 py-1 cursor-pointer mb-4 w-full max-w-xs'
           >
             Send
           </button>
