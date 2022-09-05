e5c1f529a107a150adbf96d7121921d3787df7e3
wojtek <wojtekpietraszuk@gmail.com>
2022-09-05 12:47:37 +0200

    Improve displaying a textarea text

---
+++ components/offer-details/DisplayAsTextOrList.tsx
@@ -0,0 +1,17 @@
+const DisplayAsTextOrList: React.FC<{ text: string }> = ({ text }) => {
+  if (text.split('\n').length <= 1) {
+    return <p className='lg:col-start-2 lg:col-end-3 break-words'>{text}</p>;
+  } else {
+    return (
+      <ul className='list-disc pl-7 lg:col-start-2 lg:col-end-3'>
+        {text.split('\n').map((item, index) => (
+          <li key={index} className='break-words'>
+            {item}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+};
+
+export default DisplayAsTextOrList;
--- components/offer-details/DisplayOfferDetails.tsx
+++ components/offer-details/DisplayOfferDetails.tsx
@@ -2,14 +2,21 @@ import { OfferData, FormData } from '../../helpers/types';
 import Image from 'next/image';
 import detailsBg from '../../public/images/details-bg.jpg';
 import Link from 'next/link';
+import DisplayAsTextOrList from './DisplayAsTextOrList';
 
 const DisplayOfferDetails: React.FC<{
   offer: FormData | OfferData;
   review: boolean;
 }> = ({ offer, review }) => {
-  const changeStringIntoArray = (text: string): string[] => {
-    return text.split('\n');
-  };
+  const displaySalary =
+    offer.exactSalary === ''
+      ? `${offer.minSalary} - ${offer.maxSalary}`
+      : offer.exactSalary;
+
+  const displayAddress =
+    offer.house === ''
+      ? `${offer.street} ${offer.building}, ${offer.city}`
+      : `${offer.street} ${offer.building}/${offer.house}, {offer.city}`;
 
   return (
     <div className='max-w-7xl mx-auto w-full mt-5 col-span-2'>
@@ -50,16 +57,14 @@ const DisplayOfferDetails: React.FC<{
                   <path d='M168.3 499.2C116.1 435 0 279.4 0 192 0 85.96 85.96 0 192 0c106 0 192 85.96 192 192 0 87.4-117 243-168.3 307.2-12.3 15.3-35.1 15.3-47.4 0zM192 256c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64z' />
                 </svg>
                 <span className='ml-2'>
-                  {offer.street} {offer.building}/{offer.house}, {offer.city}
+                  {displayAddress}
                 </span>
               </div>
               <div className='flex items-center text-white text-base lg:text-lg xl:text-2xl xl:my-1'>
                 <svg className='w-4 h-4 fill-white' viewBox='0 0 512 512'>
                   <path d='M320 96H192l-47.4-71.12C137.5 14.24 145.1 0 157.9 0h196.2c12.8 0 20.4 14.24 13.3 24.88L320 96zm-128 32h128c3.8 2.5 8.1 5.3 12.1 8.4C389.7 172.7 512 250.9 512 416c0 53-43 96-96 96H96c-53.02 0-96-43-96-96 0-165.1 122.3-243.3 179-279.6 4.9-3.1 9.2-5.9 13-8.4zm84.1 96c0-11.1-9-20.1-21-20.1-10.2 0-19.2 9-19.2 20.1v6c-5.6 1.2-11.8 2.9-15.9 5.1-14.9 6.8-27.9 19.4-31.1 37.7-1.8 10.2-.8 20.1 3.4 29 4.2 8.8 10.7 15 17.3 19.5 11.6 7.9 26.9 12.5 38.6 16l2.2.6c14 4.3 23.4 7.4 29.3 11.7 2.5 1.8 3.4 3.2 3.7 4.1.4.8 1 2.6.3 6.6-.6 3.5-2.5 6.5-8 8.8-6.1 2.6-16 3.9-28.8 1.9-6-1-16.7-4.6-26.2-7.8-2.2-.8-4.4-1.5-6.4-2.2-10.5-3.5-21.8 2.2-25.3 12.7s2.2 21.8 12.7 24.4c1.2 1.3 2.7 1.8 4.4 2.4 7 2.7 20.3 6.9 29.8 9.1v6.4c0 11.1 9 20.1 19.2 20.1 12 0 21-9 21-20.1v-5.5c5.3-1 10.5-3.4 15.3-4.6 15.8-6.7 28.4-19.7 31.7-38.7 1.8-10.4 1-20.4-3-29.5-3.9-9-10.2-15.6-16.9-20.4-12.1-8.9-28.3-13.7-40.4-17.4l-1.7-.2c-13.3-4.3-22.9-7.3-29-11.5-2.6-1.8-3.4-3-3.6-3.5-.2-.4-.8-1.6-.2-5 .4-2 1.9-5.3 8.2-8.1 5.6-2.9 16.4-4.5 28.6-3.5 4.4 1.6 17.9 4.2 21.8 5.2 10.6 2.9 21.6-3.5 24.4-14.2 2.9-10.6-3.5-21.6-14.2-24.4-4.4-1.2-14.4-3.2-21-4.4V224z' />
                 </svg>
-                <span className='ml-2'>
-                  {offer.minSalary}-{offer.maxSalary} PLN
-                </span>
+                <span className='ml-2'>{displaySalary} PLN</span>
               </div>
             </div>
             <div className='ml-14 lg:ml-20 xl:ml-28'>
@@ -111,116 +116,53 @@ const DisplayOfferDetails: React.FC<{
         </div>
         <hr className='hidden md:block mb-3 col-span-2' />
 
-        <div className='mt-3 flex flex-col col-span-2 md:grid md:grid-cols-2 md:mb-6'>
-          <p className='text-2xl mb-2 mt-4 col-start-1 col-end-2'>Opis</p>
-          <div className='lg:col-start-2 lg:col-end-3 break-words'>{offer.description}</div>
-        </div>
-        <hr className='hidden md:block mb-3 col-span-2' />
+        {offer.description.length !== 0 && (
+          <>
+            {' '}
+            <div className='mt-3 flex flex-col col-span-2 md:grid md:grid-cols-2 md:mb-6'>
+              <p className='text-2xl mb-2 mt-4 col-start-1 col-end-2'>Opis</p>
+              <p className='lg:col-start-2 lg:col-end-3 break-words'>
+                {offer.description}
+              </p>
+            </div>
+            <hr className='hidden md:block mb-3 col-span-2' />
+          </>
+        )}
 
         <div className='mt-3 flex flex-col col-span-2 md:grid md:grid-cols-2 md:mb-6'>
           <p className='text-2xl mb-2 mt-4 col-start-1 col-end-2'>
             Twoje podstawowe zadania
           </p>
-
-          <ul className='list-disc pl-7'>
-            {changeStringIntoArray(offer.obligation).map(
-              (obligation, index) => (
-                <li key={index} className='break-words'>{obligation}</li>
-              )
-            )}
-          </ul>
+          <DisplayAsTextOrList text={offer.obligation} />
         </div>
         <hr className='hidden md:block mb-3 col-span-2' />
 
         <div className='mt-3 flex flex-col col-span-2 md:grid md:grid-cols-2 md:mb-6'>
           <p className='text-2xl mb-2 mt-4 col-start-1 col-end-2'>Wymagania</p>
-          <ul className='list-disc pl-7'>
-            {changeStringIntoArray(offer.requirements).map(
-              (requirement, index) => (
-                <li key={index} className='break-words'>{requirement}</li>
-              )
-            )}
-          </ul>
+          <DisplayAsTextOrList text={offer.requirements} />
         </div>
         <hr className='hidden md:block mb-3 col-span-2' />
 
-        <div className='mt-3 flex flex-col col-span-2 md:grid md:grid-cols-2 md:mb-6'>
-          <p className='text-2xl mb-2 mt-4 col-start-1 col-end-2'>
-            Dobrze mieć
-          </p>
-          <ul className='list-disc pl-7'>
-            {changeStringIntoArray(offer.advantages).map((advantage, index) => (
-              <li key={index} className='break-words'>{advantage}</li>
-            ))}
-          </ul>
-        </div>
-        <hr className='hidden md:block mb-3 col-span-2' />
+        {offer.advantages.length !== 0 && (
+          <>
+            <div className='mt-3 flex flex-col col-span-2 md:grid md:grid-cols-2 md:mb-6'>
+              <p className='text-2xl mb-2 mt-4 col-start-1 col-end-2'>
+                Dobrze mieć
+              </p>
+              <DisplayAsTextOrList text={offer.advantages} />
+            </div>
+            <hr className='hidden md:block mb-3 col-span-2' />
+          </>
+        )}
 
-        <div className='mt-3 flex flex-col col-span-2 md:grid md:grid-cols-2 md:mb-6'>
-          <p className='text-2xl mb-2 mt-4 col-start-1 col-end-2'>
-            Co oferujemy
-          </p>
-          <ul className='list-disc pl-7'>
-            {changeStringIntoArray(offer.benefits).map((benefit, index) => (
-              <li key={index} className='break-words'>{benefit}</li>
-            ))}
-          </ul>
-        </div>
-
-        {/* <div className='lg:col-span-2 mt-6 bg-slate-800 px-5 rounded-t-lg pt-3 pb-3'>
-      <p className='uppercase text-2xl text-white mb-3 font-bold lg:col-start-1 lg:col-end-2'>
-        Aplikuj
-      </p>
-      <form className='flex flex-col' onSubmit={submitHandler}>
-        <div className='flex flex-col md:flex-row md:justify-between mb-4 mt-2'>
-          <div className='flex flex-col w-full md:mr-5 mb-4 md:my-0'>
-            <input
-              type='text'
-              title='name'
-              placeholder='Imie i nazwisko'
-              className='border border-white p-3 rounded-lg outline-green-500 min-w-max'
-              ref={nameRef}
-              onFocus={() => setNameError('')}
-            />
-            <small className='text-red-600 h-4'>{nameError}</small>
+        {offer.benefits.length !== 0 && (
+          <div className='mt-3 flex flex-col col-span-2 md:grid md:grid-cols-2 md:mb-6'>
+            <p className='text-2xl mb-2 mt-4 col-start-1 col-end-2'>
+              Co oferujemy
+            </p>
+            <DisplayAsTextOrList text={offer.benefits} />
           </div>
-          <div className='flex flex-col w-full md:ml-5'>
-            <input
-              type='text'
-              title='email'
-              placeholder='Twój email'
-              className='border border-white p-3 rounded-lg outline-green-500 min-w-max'
-              ref={emailRef}
-              onFocus={() => setEmailError('')}
-            />
-            <small className='text-red-600 h-4'>{emailError}</small>
-          </div>
-        </div>
-        <textarea
-          aria-label='your message to the employer'
-          placeholder='Wiadomość dla pracodawcy'
-          className='border border-white p-3 rounded-lg my-1 max-h-52 min-h-[13rem] resize-none outline-green-500'
-          maxLength={500}
-          onChange={changeMessageHandler}
-          value={message}
-        />
-        <small className='flex justify-end my-1 text-white'>
-          {message.length}/500
-        </small>
-        <input
-          type='file'
-          title='cv'
-          className='border border-white my-1 rounded-lg p-3 md:max-w-xs bg-white outline-green-500'
-          accept='.pdf, .doc, .docx'
-        />
-        <button
-          type='submit'
-          className='bg-white text-slate-800 p-3 rounded-lg mt-4 mb-1 outline-green-500 hover:bg-slate-200 transition-colors duration-200'
-        >
-          Aplikuj
-        </button>
-      </form>
-    </div> */}
+        )}
       </div>
     </div>
   );
