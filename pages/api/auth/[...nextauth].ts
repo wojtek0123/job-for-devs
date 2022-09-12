f47c0430b36047ad3926cd24f2a803e3784573f0
wojtek <wojtekpietraszuk@gmail.com>
2022-09-12 22:10:30 +0200

    Delete google provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import nextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '../../../lib/prisma';
@@ -17,10 +16,6 @@ export default nextAuth({
       clientId: process.env?.GITHUB_ID ?? '',
       clientSecret: process.env?.GITHUB_SECRET ?? '',
     }),
-    GoogleProvider({
-      clientId: process.env?.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env?.GOOGLE_CLIENT_SECRET ?? '',
-    }),
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
@@ -31,7 +26,6 @@ export default nextAuth({
         },
       },
       from: process.env.EMAIL_FROM,
-      // sendVerificationRequest,
     }),
   ],
   adapter: PrismaAdapter(prisma),
