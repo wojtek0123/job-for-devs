eeeb5cdf613added5305d9c1cecc8669a0d408e4
wojtek <wojtekpietraszuk@gmail.com>
2022-09-12 13:46:46 +0200

    Add env variable to link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
 import { ReactElement, ReactNode } from 'react';
 import { NextPage } from 'next';
 import { SessionProvider } from 'next-auth/react';
@@ -8,7 +12,7 @@ import { FiltersContextProvider } from '../context/filters-context';
 import { StepsContextProvider } from '../context/steps-context';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: `${process.env.NEXTAUTH_URL ?? ''}/api/graphql`,
   cache: new InMemoryCache(),
 });
 
