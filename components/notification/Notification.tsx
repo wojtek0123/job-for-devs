df2b7daead89007bf0c3fb82d87d8832e81a4b6a
wojtek <wojtekpietraszuk@gmail.com>
2022-10-16 11:35:38 +0200

    Add animation for notification
    Now notification are slide up and down smoothly

--- components/notification/Notification.tsx
+++ components/notification/Notification.tsx
@@ -3,19 +3,15 @@ const Notification: React.FC<{
   isError: boolean;
   show: boolean;
 }> = ({ message, isError, show }) => {
-  if (show) {
-    return (
-      <div
-        className={`fixed bottom-0 left-1/2 -translate-x-1/2 lg:transform-none px-10 py-5 text-lg text-white rounded-t-lg md:rounded-lg lg:left-5 md:bottom-5 w-full max-w-lg z-20 text-center ${
-          isError ? 'bg-red-500' : 'bg-green-500'
-        }`}
-      >
-        {message}
-      </div>
-    );
-  }
-
-  return <></>;
+  return (
+    <div
+      className={`fixed bottom-0 left-1/2 -translate-x-1/2 lg:translate-x-0 px-10 py-5 text-lg text-white rounded-t-lg md:rounded-lg lg:left-5 w-full max-w-lg z-20 text-center transition-transform duration-500 ${
+        isError ? 'bg-red-500' : 'bg-green-500'
+      } ${show ? '-translate-y-5' : 'translate-y-full'}`}
+    >
+      {message}
+    </div>
+  );
 };
 
 export default Notification;
