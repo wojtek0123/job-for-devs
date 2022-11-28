a091cf497734cf8cb53a953c76616dffd8c791b8
wojtek <wojtekpietraszuk@gmail.com>
2022-11-28 18:04:10 +0100

    Add tests for usePagination hook

---
+++ __test__/hooks/usePagination.test.ts
@@ -0,0 +1,46 @@
+import usePagination from '../../hooks/usePagination';
+import { renderHook, act } from '@/@testing-library/react';
+
+describe('usePagination hook', () => {
+  it('should return 0 when array length is 0 and take is 0', () => {
+    const { result } = renderHook(() => usePagination(0, 0));
+    expect(result.current.numberOfElements).toBe(0);
+  })
+
+  it('should return 10 when array length is not positive number but take is 10', () => {
+    const { result } = renderHook(() => usePagination(-10, 10));
+    expect(result.current.numberOfElements).toBe(0);
+  })
+
+  it('should return 10 when take argument is 10', () => {
+    const { result } = renderHook(() => usePagination(14, 10));
+    expect(result.current.numberOfElements).toBe(10);
+  })
+
+  it('should return 10', () => {
+    const { result } = renderHook(() => usePagination(14, 5));
+    act(() => {
+      result.current.showMore();
+    })
+    expect(result.current.numberOfElements).toBe(10);
+  })
+
+  it('should return array length which is 11', () => {
+    const { result } = renderHook(() => usePagination(11, 10));
+    act(() => {
+      result.current.showMore();
+      result.current.showMore();
+    })
+    expect(result.current.numberOfElements).toBe(11);
+  })
+
+  it('should return number of \'take\' argument when use showLess fn to occur situation that numberOfElements is lower than take', () => {
+    const { result } = renderHook(() => usePagination(10,5));
+    act(() => {
+      result.current.showLess();
+      result.current.showLess();
+      result.current.showLess();
+    })
+    expect(result.current.numberOfElements).toBe(10-5);
+  })
+})
